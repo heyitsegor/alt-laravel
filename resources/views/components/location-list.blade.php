e8bc707b4f5aa35f29fd8fca8a557d2281a64410
Egor Novoselov <hey.egor@icloud.com>
2023-06-30 02:01:45 +0300

    Fix editing

--- resources/views/components/location-list.blade.php
+++ resources/views/components/location-list.blade.php
@@ -1,16 +1,5 @@
 @props(['locations', 'selectedLocationId' => -1])
 
-<!-- php
-    $currentLocation = $locations[0];
-    if ($selectedLocationId == -1) {
-        $currentLocation->label = ' ';
-        $currentLocation->latitude = 0;
-        $currentLocation->longitude = 0;
-    } else {
-        $currentLocation = $locations[$selectedLocationId];
-    }
-endphp -->
-
 <div class="location-list">
     <x-location-input :selectedLocationId="$selectedLocationId" />
     @foreach ($locations as $location)
